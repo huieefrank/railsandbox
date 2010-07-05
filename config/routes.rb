69592f9212c86c8fc65c158adeb5071dccaabf8b
Frank <frank_fan@huieee.com>
2010-07-05 16:13:40 +0800

    Done with static pages excercises

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Sandbox::Application.routes.draw do |map|
   get "pages/contact"
   
   get "pages/about"
+  
+  get "pages/help"
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
